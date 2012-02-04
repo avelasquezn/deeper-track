216ef9fbd44987ef35f48475a9061f40fc16ba56
avelasquezn <angel@xendacentral.com>
2012-02-04 13:15:13 -0500

    Deeper-Track, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeeperTrack::Application.config.secret_token = 'b78b660c18210ebcac0784d1b1d68bb5850001c06876a91921f7fe4a6f7a5211e04ac70dcc841fe4ad08a5c96230143c1a66b24650e2afba549c9421e4248681'
