d1c362a6ea6a3acf592f84de736fe46c199a9a0c
avelasquezn <angel@xendacentral.com>
2012-02-09 09:19:01 -0500

    Relocation of code for repening Client and Project classes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,30 @@
 class HomeController < ApplicationController
   def index
+    @projects = nil
   	if current_user then
       if current_user.with_pivotaltracker_token?
-    		PivotalTracker::Client.use_ssl = true
-        PivotalTracker::Client.token = current_user.pivotaltracker_token
-    		PivotalTracker::Client.connection
-    		PivotalTracker::Project.class_eval do
-  				def memberships_count
-  					0 + self.memberships.all.count
-  				end
-  				def stories_count
-  					0 + self.stories.all.count
-  				end
-  			end
-    		@projects = PivotalTracker::Project.all
+        begin
+          PivotalTracker::Project.class_eval do
+            def memberships_count
+              self.memberships.all.count
+            end
+            def stories_count
+              self.stories.all.count
+            end
+          end
+          PivotalTracker::Client.class_eval do
+            def self.token(new_token)
+              @token = new_token
+            end
+          end
+      		PivotalTracker::Client.use_ssl = true         
+          PivotalTracker::Client.token(current_user.pivotaltracker_token)
+      		PivotalTracker::Client.connection
+        rescue PivotalTracker::Client::NoToken  
+          return
+        else       
+    		  @projects = PivotalTracker::Project.all
+        end  
       end
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,6 @@ module DeeperTrack
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     # Forcing the application to not access the DB or load models when precompiling the assets
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false    
   end
 end
